34ed8c2ca24a32f7a7726908a1bdd0ae4d1487bf
Lee Ren Jie <work.renjie@gmail.com>
2021-04-22 20:24:03 +0800

    initial upload for project

---
+++ LEE_REN_JIE_TP060911_LEE_YEE_HAU_TP061065.py
@@ -0,0 +1,36 @@
+# LEE REN JIE
+# TP060911
+# LEE YEE HAU
+# TP061065
+
+'''
+Functionalities of Admin
+
+i. Login to Access System.
+ii. Add Cars to be rented out.
+iii. Modify Car Details
+iv. Display All records of
+a. Cars Rented Out
+b. Cars available for Rent
+c. Customer Bookings
+d. Customer Payment for a specific time duration
+v. Search Specific record of
+a. Customer Booking
+b. Customer Payment
+vi. Return a Rented Car.
+vii. Exit
+
+Functionalities of All Customers (Registered / Not-Registered)
+i. View all cars available for rent.
+ii. New customer Register to Access other Details
+iii. Exit
+
+Functionalities of Registered Customer
+i. Login to Access System
+ii. Modify Personal Details.
+iii. View Personal Rental History.
+iv. View Detail of Cars to be Rented Out.
+v. Select and Book a car for a specific duration.
+vi. Do payment to confirm Booking.
+vii. Exit
+'''
